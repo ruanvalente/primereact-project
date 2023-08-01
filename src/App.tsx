9aac031a69af4a9f41e414739698edef834f085a
ruanvalente <ruanopensuselinux@gmail.com>
2023-07-31 21:11:01 -0300

    style: add custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "primereact/button";
+
 export function App() {
   return (
     <div>
       <h2>PrimeReact Project</h2>
+      <Button label="Check" icon="pi pi-check" />
     </div>
   );
 }
